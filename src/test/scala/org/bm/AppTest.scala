182971926712a276d7f27cb04ca247805c05a325
Numan KARABIYIK <yvznmn@gmail.com>
2023-03-07 18:56:41 -0700

    initial push

---
+++ src/test/scala/org/bm/AppTest.scala
@@ -0,0 +1,17 @@
+package org.bm
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
